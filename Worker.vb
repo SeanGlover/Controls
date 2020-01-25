734181d029a168522b8b82a6aee1a24570bc215a
Sean Glover <sglover@ca.ibm.com>
2020-01-25 13:06:59 -0500

    worker class

---
+++ Worker.vb
@@ -0,0 +1,9 @@
+﻿Option Explicit On
+Option Strict On
+Public NotInheritable Class Worker
+    Inherits ComponentModel.BackgroundWorker
+    Public Sub New()
+    End Sub
+    Public Property Tag As Object
+    Public Property Name As String
+End Class
